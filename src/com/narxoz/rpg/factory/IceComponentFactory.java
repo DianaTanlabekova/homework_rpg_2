1c42f421c7cd47d039ce1b34a70fb91f9737e690
DianaTanlabekova <diana.tanlabekova@narxoz.kz>
2026-02-18 08:55:32 +0500

    wrote icecomponentfactory

---
+++ src/com/narxoz/rpg/factory/IceComponentFactory.java
@@ -0,0 +1,27 @@
+package com.narxoz.rpg.factory;
+
+import com.narxoz.rpg.combat.*;
+import com.narxoz.rpg.loot.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IceComponentFactory implements EnemyComponentFactory {
+    @Override
+    public List<Ability> createAbilities() {
+        List<Ability> abilities = new ArrayList<>();
+        abilities.add(new FrostBreath("Eternal Zero"));
+        abilities.add(new IceShield("Glacial Shell"));
+        abilities.add(new Blizzard("Winter's Wrath"));
+        return abilities;
+    }
+
+    @Override
+    public LootTable createLootTable() {
+        return new IceLootTable();
+    }
+
+    @Override
+    public String createAIBehavior() {
+        return "DEFENSIVE";
+   }
+}
